ef8cf3a5ea307363b03a63cee81aeba600d49d57
NikHighland <NikAlmonte@yahoo.com>
2021-11-20 17:02:51 -0500

    PMBR Stagelist rework + Music

--- Build/Project+/Source/Stagelists/PMBRStagelist.asm
+++ Build/Project+/Source/Stagelists/PMBRStagelist.asm
@@ -35,72 +35,74 @@ CODE @ $800B91C8
 .GOTO->SkipStageTables
 
 TABLE_1:
-	byte[21] |
-0x24, | # Peach's Castle
-0x0E, | # Lylat Cruise
-0x0C, | # Yoshi's Island
-0x05, | # Bowser's Castle
-0x21, | # Golden Temple
-0x1E, | # Sky Sanctuary Zone
-0x09, | # Hyrule Castle
-0x14, | # Castle Siege
-0x15, | # Wario Land
-0x18, | # Fountain of Dreams
-0x01, | # Final Destination
-0x04, | # Metal Cavern
-0x23, | # Dream Land
-0x0A, | # Metroid Lab
-0x20, | # Yoshi's Story
-0x1C, | # Green Hill Zone
-0x1A, | # Smashville
-0x00, | # Battlefield
-0x28, | # Pokemon Stadium 2
-0x02, | # Delfino's Secret
-0x16  | # Distant Planet
+	byte[10] | 
+ 0x15, |
+ 0x18, |
+ 0x01, |
+ 0x04, |
+ 0x23, |
+ 0x1C, |
+ 0x1A, |
+ 0x00, |
+ 0x28, |
+ 0x02 |
+
 
 TABLE_2:
-	byte[21] |
-0x12, | # Infinite Glacier
-0x1B, | # Shadow Moses Island
-0x06, | # Kongo Jungle
-0x19, | # Fourside
-0x22, | # Onett
-0x26, | # Big Blue
-0x11, | # Port Town Aero Dive
-0x13, | # Flat Zone 2
-0x03, | # Luigi's Mansion
-0x07, | # Rumble Falls
-0x25, | # Corneria
-0x27, | # Planet Zebes
-0x1F, | # Temple
-0x08, | # Pirate Ship
-0x2B, | # Training Room
-0x10, | # Spear Pillar
-0x0F, | # Saffron City
-0x0D, | # Halberd
-0x0B, | # Frigate Orpheon
-0x17, | # Skyworld
-0x1D  | # PictoChat
+	byte[24] | 
+ 0x07, |
+ 0x20, |
+ 0x0A, |
+ 0x14, |
+ 0x05, |
+ 0x21, |
+ 0x1E, |
+ 0x0C, |
+ 0x0E, |
+ 0x06, |
+ 0x1B, |
+ 0x19, |
+ 0x16, |
+ 0x12, |
+ 0x26, |
+ 0x11, |
+ 0x13, |
+ 0x22, |
+ 0x03, |
+ 0x25, |
+ 0x2B, |
+ 0x1F, |
+ 0x10, |
+ 0x0F |
+
 
 TABLE_3:
-	byte[17] |
-0x31, | # Dinosaur Land
-0x2D, | # Mario Circuit
-0x38, | # Mushroom Kingdom
-0x3B, | # Rainbow Cruise
-0x32, | # Oil Drum Alley
-0x33, | # Jungle Japes
-0x2E, | # Clock Town
-0x36, | # Cookie Country
-0x39, | # WarioWare, Inc.
-0x3C, | # Poke Floats
-0x34, | # Bell Tower
-0x35, | # Norfair
-0x2F, | # Hanenbow
-0x37, | # Venus Lighthouse
-0x2C, | # Dracula's Castle
-0x30, | # Dead Line
-0x3A  | # Subspace
+	byte[25] | 
+ 0x31, |
+ 0x2D, |
+ 0x38, |
+ 0x3B, |
+ 0x32, |
+ 0x33, |
+ 0x2E, |
+ 0x36, |
+ 0x24, |
+ 0x39, |
+ 0x3C, |
+ 0x27, |
+ 0x08, |
+ 0x0D, |
+ 0x34, |
+ 0x35, |
+ 0x2F, |
+ 0x09, |
+ 0x37, |
+ 0x1D, |
+ 0x2C, |
+ 0x30, |
+ 0x3A, |
+ 0x17, |
+ 0x0B |
 
 TABLE_4:	# Unused
 TABLE_5:	# Unused
@@ -127,12 +129,12 @@ half[61] |	# Stage Count + 2
 0x4C2F, 0x4D30, 0x4E31, 0x4F3D, | # Mushroom Kingdom, WarioWare, Subspace, Rainbow Cruise
 0x503E				| # Poke Floats
 
+
 SkipStageTables:
 .RESET
-* 26523400 00000002 # If 80523400 is less than 2
-byte 21 @ $806B929C # Page 1
-byte 21 @ $806B92A4 # Page 2
-byte 17 @ $80496002 # Page 3
+byte 10 @ $806B929C # Page 1
+byte 24 @ $806B92A4 # Page 2
+byte 25 @ $80496002 # Page 3
 byte 00 @ $80496003 # Page 4 (Unused)
 byte 00 @ $80496004 # Page 5 (Unused)
 byte 59 @ $800AF673 # Stage Count
